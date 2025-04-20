e0b5909ae232d646359e852ca96f3384296f9caa
20www02 <avstruin@gmail.com>
2025-04-20 15:28:50 +0300

    ClassWork

---
+++ js-intro/js/2025-04-20.js
@@ -0,0 +1,37 @@
+// 1.
+function sqeare(base) {
+    return base * base;
+}
+function sqeare(base) {
+    return sqeare(base) * base;
+}
+
+// 2.
+function rectangular_parallelepiped_volume(length, width, height) {
+    return (length * width) * height;
+}
+
+// 3.
+function pow(base, exp) {
+    if(!exp == 0 && base == 0) return undefined;
+    let result = 1; 
+    for (let cur_exp = 1; cur_exp <= exp; ++cur_exp) {
+        result *= base;
+    }
+}
+
+
+// рекурсивный вариант
+function pow_rec(base, exp) {
+    if (exp == 0 && base == 0 || exp < 0) return undefined;
+    if (exp == 0) return 0;
+    if (base == 0 || base == 1) return base;
+    return base * pow_rec(base, exp - 1)
+}
+
+// (рекурсивно) факториал числа
+function factor(n) {
+    if (n < 0) return;
+    if (n == 0) return 1;
+    return n * factor(n - 1);
+}
